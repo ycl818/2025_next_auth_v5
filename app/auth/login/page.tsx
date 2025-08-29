58108a9a0c8511f76f75a692f79ca51aa2b2b90d
yichien <kevinyichienlee@gmail.com>
2025-08-30 00:31:02 +0800

    start to create a card wrapper

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,16 @@
+import { CardWrapper } from "@/components/auth/card-wrapper";
 import { LoginForm } from "@/components/auth/login-form";
 
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <LoginForm />
+    </CardWrapper>
+  );
 };
 export default LoginPage;
---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Header } from "@/components/auth//header";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      {children}
+    </Card>
+  );
+};
