9f31ce412de0eca67fa26d45e630fe4f5f418731
yichien <kevinyichienlee@gmail.com>
2025-08-29 23:59:16 +0800

    add login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,53 @@
+import { Poppins } from "next/font/google";
+
 import { Button } from "@/components/ui/button";
 
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
-  return <Button className="lg">Click me</Button>;
+  return (
+    <>
+      <main
+        className="flex h-full flex-col items-center justify-center 
+    
+    "
+        style={{
+          backgroundImage: `
+          radial-gradient(circle at 50% 100%, rgba(253, 224, 71, 0.4) 0%, transparent 60%),
+          radial-gradient(circle at 50% 100%, rgba(251, 191, 36, 0.4) 0%, transparent 70%),
+          radial-gradient(circle at 50% 100%, rgba(244, 114, 182, 0.5) 0%, transparent 80%)
+        `,
+        }}
+      >
+        <div className="space-y-6 text-center">
+          <h1
+            className={cn(
+              "text-6xl font-semibold text-white drop-shadow-md",
+              font.className
+            )}
+          >
+            🔐 Auth
+          </h1>
+          <p className="text-white text-lg">A simple authentication service</p>
+          <div>
+            <LoginButton>
+              <Button variant="secondary" size="lg">
+                Sign in
+              </Button>
+            </LoginButton>
+            {/* <LoginButton asChild>
+              <Button variant="secondary" size="lg">
+                Sign in
+              </Button>
+            </LoginButton> */}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface LoginBottonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export function LoginButton({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginBottonProps) {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO: Implement modal</span>;
+  }
+
+  return (
+    <span className="cursor-pointer" onClick={onClick}>
+      {children}
+    </span>
+  );
+}
