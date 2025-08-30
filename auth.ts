6e509a5bf212d48e3d4e4de18f1f52a0654377eb
yichien <kevinyichienlee@gmail.com>
2025-08-30 23:48:02 +0800

    set a good middleware and use settings as example page to test OK

--- auth.ts
+++ auth.ts
@@ -4,12 +4,7 @@ import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 
-export const {
-  handlers: { GET, POST },
-  signIn,
-  signOut,
-  auth,
-} = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" }, // prisma doesn't work in the edge, we cannot use the database session strategy
   ...authConfig,
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,40 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+} from "./routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
   console.log("Route", req.nextUrl.pathname);
   console.log("IS LoggedIn:", isLoggedIn);
+  return null;
 });
 
 // Optionally, don't invoke Middleware on some paths 除了以下位置 其他都會執行auth驗證
