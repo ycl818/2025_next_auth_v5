6b99a8d8c0962b1760414dedc6de8febfa8da0ab
yichien <kevinyichienlee@gmail.com>
2025-08-30 22:33:04 +0800

    instantiate a standalone client without the adapter for middleware and another one to be used everywhere else

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
-export { auth as middleware } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
 
-// Optionally, don't invoke Middleware on some paths
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("Route", req.nextUrl.pathname);
+  console.log("IS LoggedIn:", isLoggedIn);
+});
+
+// Optionally, don't invoke Middleware on some paths 除了以下位置 其他都會執行auth驗證
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
