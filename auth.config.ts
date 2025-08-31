332e0b140f1e163a306e0586fccab363dd82a3fc
yichien <kevinyichienlee@gmail.com>
2025-08-31 13:44:00 +0800

    add oauth for google and github test OK

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,23 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "./data/user";
-import bcrypt from "bcryptjs";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       authorize: async (credentials) => {
         const validatedFields = LoginSchema.safeParse(credentials);
