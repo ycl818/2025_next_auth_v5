d9236d26de1c9a026e419df67c24e6aaecf18522
yichien <kevinyichienlee@gmail.com>
2025-09-11 23:37:52 +0800

    remove logout btn in settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,26 +1,11 @@
 "use client";
 
-import { logout } from "@/actions/logout";
-import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
 const SettingsPage = () => {
   const user = useCurrentUser();
 
-  const onClick = () => {
-    logout();
-  };
-
-  return (
-    <div>
-      {JSON.stringify(user)}
-      <form>
-        <Button onClick={onClick} type="button">
-          Sign out
-        </Button>
-      </form>
-    </div>
-  );
+  return <div>{JSON.stringify(user)}</div>;
 };
 
 export default SettingsPage;
